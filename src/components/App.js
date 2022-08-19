9cbd33538407d964976cbdbc809297dce1a24311
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-20 00:55:50 +0200

    Customizing Form Fields

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import StreamList from "./streams/StreamList";
 import Header from "./Header";
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
           <Header />
