1f7dad8196cab5fdf52c770c26994abcc4aee811
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-21 01:54:30 +0200

    Create Stream Action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+
 import {
   legacy_createStore as createStore,
   applyMiddleware,
@@ -9,7 +11,10 @@ import {
 import reducers from "./reducers";
 import App from "./components/App";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 root.render(
